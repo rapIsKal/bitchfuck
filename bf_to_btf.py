7ee36c33564bed0139fec4d910a74f07e51795ee
Lil Pyth On <lp@tsidaev.ru>
2019-08-01 17:41:24 +0500

    bf_to_btf.py: add hash[gang]bang, add usage instructions, ignore formatting

--- bf_to_btf.py
+++ bf_to_btf.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
 import sys
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("c'mon run me as bf_to_btf.py <input.bf>")
+        exit(1)
+        
     PERMITTED_KEYWS = {'+': 'BITCH', '<': 'NIGGA', '-': 'FUCK', '[': 'STUFF',
                        ']': 'MOTHERFUCKER', '>': 'YO', ',': 'RIDE', '.': 'SHIT'}
     out_file = sys.argv[1].split('.')[0] + '.btf'
@@ -9,6 +14,9 @@ if __name__ == '__main__':
     data = list(data)
     data_btf = []
     for s in data:
+        # Ignore formatting
+        if s in [ '\n', '\r', ' ', '\t' ]:
+            continue
         data_btf.append(PERMITTED_KEYWS[s])
     with open(out_file, 'w') as f:
         f.write('\n'.join(data_btf))
